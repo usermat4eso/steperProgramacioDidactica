3e0a4f53fb57929edbb098d3926cc5f71fd3af6c
usermat4eso <125912364+usermat4eso@users.noreply.github.com>
2025-09-04 07:16:50 +0200

    feat: Update step images and titles
    Replaces external image URLs with local assets for steps 1-4. Updates the title of Step 1 to reflect a download action.

--- constants.ts
+++ constants.ts
@@ -7,26 +7,26 @@ import Step4 from './components/steps/Step4';
 export const STEPS: Step[] = [
   {
     id: 1,
-    title: 'Paso 1: Configuración del Entorno',
+    title: 'Paso 1: Descarga archivo',
     component: Step1,
-    imageUrl: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    imageUrl: './assets/images/step1.jpeg',
   },
   {
     id: 2,
     title: 'Paso 2: Importación de Datos',
     component: Step2,
-    imageUrl: 'https://images.pexels.com/photos/3184292/pexels-photo-3184292.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    imageUrl: './assets/images/step2.jpeg',
   },
   {
     id: 3,
     title: 'Paso 3: Procesamiento de Datos',
     component: Step3,
-    imageUrl: 'https://images.pexels.com/photos/590022/pexels-photo-590022.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    imageUrl: './assets/images/step3.jpeg',
   },
   {
     id: 4,
     title: 'Paso 4: Visualización de Resultados',
     component: Step4,
-    imageUrl: 'https://images.pexels.com/photos/669615/pexels-photo-669615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    imageUrl: './assets/images/step4.jpeg',
   },
 ];
